8af3ba2391a3bb479f18ac9407e47c3f4a16846c
Rafał Schmidt <rafalschmidt97@gmail.com>
2020-11-22 02:51:50 +0100

    Updates

---
+++ precache-manifest.b58e08ebf8bd92a729c412c5ee81450b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "552e84b4482d94361d50f0e536242fc1",
+    "url": "/will-energy/index.html"
+  },
+  {
+    "revision": "00ad7ab704ca4ac48f6e",
+    "url": "/will-energy/static/css/2.2c28e4d7.chunk.css"
+  },
+  {
+    "revision": "1e256003086940469b21",
+    "url": "/will-energy/static/css/main.b9261224.chunk.css"
+  },
+  {
+    "revision": "00ad7ab704ca4ac48f6e",
+    "url": "/will-energy/static/js/2.f8def2f1.chunk.js"
+  },
+  {
+    "revision": "9de05c2ec2c1b1fdbbc52fdff90093a3",
+    "url": "/will-energy/static/js/2.f8def2f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e256003086940469b21",
+    "url": "/will-energy/static/js/main.6e34db51.chunk.js"
+  },
+  {
+    "revision": "39c04eca798e47f9d4ef",
+    "url": "/will-energy/static/js/runtime-main.611bfdf2.js"
+  },
+  {
+    "revision": "c9b1d6e344c2c0b1ed467db9ba5f4fce",
+    "url": "/will-energy/static/media/img-calculator.c9b1d6e3.svg"
+  },
+  {
+    "revision": "761931d9edac6930164dc1304d0b8655",
+    "url": "/will-energy/static/media/img-person-bounds.761931d9.svg"
+  }
+]);
